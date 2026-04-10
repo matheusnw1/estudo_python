385b39e973dcb44d6836b0e1f25d683bf374b701
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-04-10 18:35:40 -0300

    Update spot.py
    Refactor music search logic into a function.

--- spot.py
+++ spot.py
@@ -1,20 +1,11 @@
-
 import requests
 
-musica = requests.get('https://itunes.apple.com/search?term=ogrilo&media=music&limit=5')
-
-dicionario = (musica.json())
-
-musicas = (dicionario['results'])
-
-decisao = input('Qual artista você quer ouvir?: ')
-# if decisao:
-
-
-
-
-i = 1
-
-for musica in musicas:
-    print(f'Musica {i}:  {musica['trackName']}')
-    i += 1
+def buscar_musicas(artista):
+    lista = []
+    musica = requests.get(f'https://itunes.apple.com/search?term={artista}&media=music&limit=5')
+    dicionario = (musica.json())
+    musicas = (dicionario['results'])
+    for musica in musicas:
+        music = (f'{musica['trackName']}')
+        lista.append(music)
+    return lista
