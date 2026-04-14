4b7c4d601db76df754700ad8cc43ca5b93532965
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-04-13 23:47:28 -0300

    Update caixa_eletronico.py

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -1,1 +1,20 @@
 
+valor = 286
+
+notas = [100, 50, 20, 10, 5, 1]
+resultado = {}
+
+resto = valor
+
+for nota in notas:
+    quantidade = resto // nota
+    resultado[nota] = quantidade
+    resto = resto % nota
+
+print("Valor:", valor)
+print("Notas utilizadas:")
+
+for nota, qtd in resultado.items():
+    if qtd > 0:
+        print(f"{qtd} nota(s) de {nota}")
+      
