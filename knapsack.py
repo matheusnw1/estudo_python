0ddbf6b78af4572d55e22c6bc263916f7bc1af45
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-04-25 21:38:39 -0300

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,1 +1,20 @@
 
+pesos = [2, 3, 4, 5]
+valores = [3, 4, 5, 6]
+capacidade = 5
+
+n = len(pesos)
+
+dp = [[0 for _ in range(capacidade + 1)] for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for w in range(capacidade + 1):
+        if pesos[i - 1] <= w:
+            dp[i][w] = max(
+                valores[i - 1] + dp[i - 1][w - pesos[i - 1]],
+                dp[i - 1][w]
+            )
+        else:
+            dp[i][w] = dp[i - 1][w]
+
+print("Valor máximo:", dp[n][capacidade])
