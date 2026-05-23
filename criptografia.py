0b28c98d95cb535a01df879ed76a4c2aeedc629e
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-05-22 23:36:13 -0300

    Update criptografia.py

--- criptografia.py
+++ criptografia.py
@@ -1,1 +1,23 @@
 
+import string
+
+alfabeto = string.ascii_lowercase
+deslocamento = 3
+
+texto = input("Digite uma mensagem: ").lower()
+
+resultado = ""
+
+for letra in texto:
+
+    if letra in alfabeto:
+
+        posicao = alfabeto.index(letra)
+        nova_posicao = (posicao + deslocamento) % 26
+
+        resultado += alfabeto[nova_posicao]
+
+    else:
+        resultado += letra
+
+print(f"Texto criptografado: {resultado}")
