6930b0952a0230bce99b989cdbae2a2fd31b2f2c
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-05-27 23:56:37 -0300

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -1,1 +1,22 @@
 
+import psutil
+import time
+
+while True:
+
+    cpu = psutil.cpu_percent(interval=1)
+    memoria = psutil.virtual_memory().percent
+
+    print(f"CPU: {cpu}%")
+    print(f"Memória: {memoria}%")
+
+    if cpu > 80:
+        print("Uso de CPU muito alto!")
+
+    if memoria > 80:
+        print("Uso de memória muito alto!")
+
+    print("-" * 30)
+
+    time.sleep(2)
+  
